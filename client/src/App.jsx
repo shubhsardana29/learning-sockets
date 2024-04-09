d70332bcbdf8bc4207d6c132dddf0b1049ada52b
shubhsardana29 <shubhsardana29@gmail.com>
2024-04-09 12:12:09 +0530

    Add dependencies and enable JWT authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,9 @@ import {
 } from "@mui/material";
 
 const App = () => {
-  const socket = useMemo(() => io("http://localhost:3000"), []);
+  const socket = useMemo(() => io("http://localhost:3000", {
+    withCredentials: true,
+  }), []);
 
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState("");
--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import jwt from "jsonwebtoken";
 import cors from "cors";
-import { Socket } from "dgram";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3000;
+
+const secretKeyJWT = "randomsecretkey";
 
 // Create an HTTP server using Express app
 const server = createServer(app);
@@ -20,33 +23,55 @@ const io = new Server(server, {
 });
 
 // Set up basic CORS for Express app (not needed anymore)
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST'],
-//     credentials: true,
-// }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected",socket.id);
-  
-    socket.on("message", (data) => {
-        console.log(data);
-        socket.to(data.room).emit("received-message", data.message);
-    })
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "random123" }, secretKeyJWT);
+  res
+    .cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+    .json({
+      message: "Logged in",
+    });
+});
 
-    socket.on("join-room", (room) => {
-        console.log("joining room", room);
-        socket.join(room);
-        console.log(`user joined ${room}`);
-    });
-    
-    socket.on("disconnect", () => {
-        console.log("user disconnected" , socket.id);
-    });
+
+io.use((socket, next) => {
+  cookieParser()(socket.request, socket.request.res, (err) => {
+    if (err) return next(err);
+    const token = socket.request.cookies.token;
+      if (!token) return next(new Error("Authentication error"))
+      const decoded = jwt.verify(token, secretKeyJWT);
+      next();
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("received-message", data.message);
+  });
+
+  socket.on("join-room", (room) => {
+    console.log("joining room", room);
+    socket.join(room);
+    console.log(`user joined ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
 });
 
 server.listen(port, () => {
